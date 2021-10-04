195739ae370d95c0534622bbb29130462205fb95
Kyle Ortiz <kyleaortiz@gmail.com>
2021-10-04 15:20:46 -0500

    commit tree eea312e906a496e4c262fd9fbbf6132d30e01e03

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,12 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  
+  def show
+    cheese = Cheese.find_by(id: params[:id])
+
+    render json: cheese
+    
+  end
 
 end
